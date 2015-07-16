81c4819e93f4f6c98fd81c608cf9c9b3e6d24d6a
kareem1234 <kareem.lewis79@gmail.com>
2015-07-16 15:39:04 -0400

    added

---
+++ platforms/android/com.google.playservices/app-google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/kareemlewis/Documents/GitHub/bantter-app/platforms/android/com.google.playservices/app-google-play-services_lib/bin/AndroidManifest.xml : \
+/Users/kareemlewis/Documents/GitHub/bantter-app/platforms/android/com.google.playservices/app-google-play-services_lib/AndroidManifest.xml \
